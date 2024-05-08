cb602768b11e38f12bbffe6e8424ab38b117b15d
WB@AHM <WB@AHM>
2024-05-08 11:23:45 +0200

    branch start

--- test/address_book_tests.cpp
+++ test/address_book_tests.cpp
@@ -3,11 +3,23 @@
 
 #include "address_book.hpp"
 
-TEST_CASE("entries can be added and removed") {
+TEST_CASE("entries can be added and removed")
+{
 	address_book ab;
 	CHECK_FALSE(ab.has_entry("Jane Doe"));
 	ab.add_entry("Jane Doe");
 	CHECK(ab.has_entry("Jane Doe"));
+
+	ab.set_phone_number("Jane Doe", 23);
+	CHECK(ab.get_phone_number( "Jane Doe") == 23);
+
 	ab.remove_entry("Jane Doe");
 	CHECK_FALSE(ab.has_entry("Jane Doe"));
+
+char	long_entry[] = "A012345679 B012345679 C012345679 D012345679 E012345679 F012345679 G012345679 H012345679 I012345679 "
+		       "A012345679 B012345679 C012345679 D012345679 E012345679 F012345679 G012345679 H012345679 I012345679 "
+		       "A012345679 B012345679 C012345679 D012345679 E012345679 F012345679 G012345679 H012345679 I012345679 ";
+
+	CHECK_THROWS( ab.add_entry(long_entry) );
+	CHECK_FALSE(ab.has_entry(long_entry));
 }
